a3a41eb7458f7cc04bb53437967bcdf2b70f742f
NandreyN <nandreyn1998@gmail.com>
2017-04-05 01:10:58 +0300

    App skeleton

---
+++ StudyPracticeTestApplication/Source.cpp
@@ -0,0 +1,109 @@
+﻿#undef UNICODE
+#include <windows.h>
+#include  <math.h>
+
+using namespace std;
+
+BOOL InitWnd1(HINSTANCE hinstance);
+BOOL InitInstance1(HINSTANCE hinstance, int nCmdShow);
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
+
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE prevHinstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	MSG msg;
+
+	if (!InitWnd1(hinstance))
+	{
+		MessageBox(NULL, "Unable to Init App", "Error", MB_OK);
+		return FALSE;
+	}
+
+	if (!InitInstance1(hinstance, nCmdShow))
+	{
+		MessageBox(NULL, "Unable to Init Instance", "Error", MB_OK);
+		return FALSE;
+	}
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+BOOL InitWnd1(HINSTANCE hinstance)
+{
+	WNDCLASSEX wndclass;
+	wndclass.cbSize = sizeof(wndclass);
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hinstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_CROSS);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = "Test";
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wndclass))
+	{
+		MessageBox(NULL, "Cannot register class", "Error", MB_OK);
+		return FALSE;
+	}
+	return TRUE;
+}
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
+{
+	static int x, y;
+	static HDC hdc;
+	static bool isDirect, moves;
+
+	switch (message)
+	{
+	case WM_CREATE:
+		break;
+	case WM_SIZE:
+		x = LOWORD(lparam);
+		y = HIWORD(lparam);
+		break;
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	case WM_RBUTTONUP:
+		moves = !moves;
+		break;
+	default:
+		return DefWindowProc(hwnd, message, wparam, lparam);
+	}
+	return FALSE;
+}
+
+BOOL InitInstance1(HINSTANCE hinstance, int nCmdShow)
+{
+	HWND hwnd;
+	hwnd = CreateWindow(
+		"Test",
+		"Test",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		NULL,
+		NULL,
+		hinstance,
+		NULL);
+
+
+	if (!hwnd)
+		return FALSE;
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	return TRUE;
+}
